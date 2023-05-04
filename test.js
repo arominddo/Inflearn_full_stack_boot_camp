bdef96d9bf28fba4dfa0a40c0105f64267d52277
mingddo <godhsb@naver.com>
2023-05-04 15:18:58 +0900

    2023.05.03.(1)

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-console.log("hello inflearn!");
+const grab = {
+    name: "그랩",
+    age : 27
+};
+const people = ["민수", "철수"];
+
+let name = grab.name;
+let age = grab.age;
+let minsoo = people[0];
+let chul = people[1];
+
+
+const [p1, p2, p3] = people;
+
+
+//console.log(p1);
+
+people.forEach((a,b,c) => {console.log(c)})
+
