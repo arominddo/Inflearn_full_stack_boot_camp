26eb31f7fb9aa60ad5241ba182d384f0e94828c8
mingddo <godhsb@naver.com>
2023-05-04 17:41:02 +0900

    2023.05.04.(1)

--- grab_market_web/src/App.js
+++ grab_market_web/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import AaA from './child.js';
+import TimerComponent from './timer';
+
 
 function App() {
+  
+  const text = "인프런 수강생 여러분 화이팅!";
+  const hell = function (){
+    return (<h3>인프런 강의 너무 좋아</h3>);
+  }
+
+  const sayhell2 = function(){
+    alert("어어어어어어");
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> 안녕하세요 </h1>
+      <h2> {text} </h2>
+      {hell()}
+      <div onClick={sayhell2}> 클릭해 주세요.</div>
+      <TimerComponent />
+
+      <AaA name={"로로"} age={27}/>
+      <AaA age={27} name={"바바바"}/>
+      
+
     </div>
   );
 }
---
+++ grab_market_web/src/child.js
@@ -0,0 +1,19 @@
+function AaA(props){
+
+    const {ff, aa} = props;
+    const gg = props.name;
+    const dd = props.age;
+
+    return (
+        <div>
+            <p>
+                이름 {ff} 나이 {dd} asdfsafasas
+            </p>
+        </div>
+        );
+
+}
+
+export default AaA;
+
+
