dd712286a6118c240aa6d1d20c6b72da9b86686e
mingddo <godhsb@naver.com>
2023-05-19 16:47:51 +0900

    2023.05.19.(1)

--- grab_market_web/src/main/index.js
+++ grab_market_web/src/main/index.js
@@ -28,23 +28,34 @@ function MainPage() {
                 </div>
                 <h1>판매되는 상품들</h1>
                 <div id="product-list">
-                    <div className="product-card">
-                        <div>
-                            <img className="product-img" src="images/products/keyboard1.jpg" />
-                        </div>
-                        <div className="product-contents">
-                            <span className="product-name">
-                                키보드
-                            </span>
-                            <span className="product-price">
-                                50000원
-                            </span>
-                            <div className="product-seller">
-                                <img className="product-avatar" src="images/icons/avatar.png" />
-                                <span>그랩</span>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        products.map(function(product, index) {
+                            return (
+                              <div className="product-card">
+                                <div>
+                                  <img
+                                    className="product-img"
+                                    src="images/products/keyboard1.jpg"
+                                  />
+                                </div>
+                                <div className="product-contents">
+                                  <span className="product-name">키보드</span>
+                                  <span className="product-price">50000원</span>
+                                  <div className="product-seller">
+                                    <img
+                                      className="product-avatar"
+                                      src="images/icons/avatar.png"
+                                    />
+                                    <span>그랩</span>
+                                  </div>
+                                </div>
+                              </div>
+                            );
+                            
+
+                        })
+                    }
+                    
 
                 </div>
             </div>
