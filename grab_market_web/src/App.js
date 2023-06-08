584422cd885f23c6ace36e7bdef8daed9e950ebf
mingddo <godhsb@naver.com>
2023-06-08 17:44:13 +0900

    2023.06.08.(1)

--- grab_market_web/src/App.js
+++ grab_market_web/src/App.js
@@ -1,14 +1,41 @@
 // import logo from './logo.svg';
 import './App.css';
 import MainPageComponent from './main/index.js';
-
+import {Routes, Route} from 'react-router-dom';
+import UploadPage from "./upload";
+import ProductPage from './product';
 
 function App() {
   
  
+  // return (
+  //   <div>
+  //     <Routes>
+  //       <Route exact={true} path="/">
+  //         <MainPageComponent />
+  //       </Route>
+  //       <Route exact={true} path="/product">
+  //         <ProductPage />
+  //       </Route>
+  //       <Route exact={true} path="/upload">
+  //         <UploadPage />
+  //       </Route>
+  //     </Routes>
+  //   </div>
+  // );
+
+
   return (
-    <MainPageComponent />
+    <div>
+      <Routes>
+        <Route exact={true} path="/" element={<MainPageComponent />} />
+        <Route exact={true} path="/products/:id" element={<ProductPage />} />
+        <Route exact={true} path="/upload" element={<UploadPage />} />
+      </Routes>
+    </div>
   );
+
+
 }
 
 export default App;
