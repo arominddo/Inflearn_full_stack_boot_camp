fe5a2292298f6ac48dd447f749eb11a568ab6f33
mingddo <godhsb@naver.com>
2023-06-23 15:12:55 +0900

    2023.06.23.(1)_상품 생성, 조회 API 구현

--- grab_market_server/server.js
+++ grab_market_server/server.js
@@ -1,3 +1,4 @@
+const models = require("./models")
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -8,46 +9,48 @@ app.use(cors());
 
 app.get("/products", (req,res) => {
 
+    models.Product.findAll().then((result) => {
+        console.log("PRODUCTS : ", result);
+        
+        res.send({
+            products : result
+        });
+    }).catch((error) => {
+        console.error(error);
+        res.send("에러 발생");
+
+    });
+
     const query = req.query;
 
-    res.send({
-        "products" : [
 
-            {
-                "id" : 1,
-              "name": "농구공",
-              "price": 100000,
-              "seller": "조던",
-              "imageUrl": "images/products/basketball1.jpeg"
-            },
-            {
-                "id": 2,
-              "name": "축구공",
-              "price": 50000,
-              "seller": "메시",
-              "imageUrl": "images/products/soccerball1.jpg"
-            },
-            {
-                "id" : 3,
-              "name": "키보드",
-              "price": 10000,
-              "seller": "그랩",
-              "imageUrl": "images/products/keyboard1.jpg"
-            }
-
-        ]
-
-        
-    });
 });
 
 app.post("/products", (req, res) => {
 
     const body = req.body;
+    const {name, description, price, seller} = body;
 
-    res.send({
-        body
+    if(!name || !description || !price || !seller){
+        res.send("모든 필드를 입력해주세요.")
+
+    }
+
+    models.Product.create({
+        name,
+        description,
+        price,
+        seller
+    }).then((result) => {
+        console.log("상품 생성 결과 : ", result);
+        res.send({
+            result,
+        })
+    }).catch((error) => {
+        console.error(error);
+        res.send("상품 업로드에 문제가 발생했습니다.");
     })
+
 })
 
 app.get("/products/:id", (req, res) => {
@@ -57,5 +60,14 @@ app.get("/products/:id", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다.")
-})
+    console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다.");
+    models.sequelize.sync()
+    .then(() => {
+        console.log('✓ DB 연결 성공');
+    })
+    .catch(function(err) {
+        console.error(err);
+        console.log('✗ DB 연결 에러');
+        process.exit();
+    });
+});
