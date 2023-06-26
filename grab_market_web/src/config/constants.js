a26b3f59866d41051cd94073afc3866f780284dd
mingddo <godhsb@naver.com>
2023-06-26 19:24:57 +0900

    2023.06.26.(3)_Client_Web_배포

--- grab_market_web/src/config/constants.js
+++ grab_market_web/src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL  = "http://localhost:8080";
+export const API_URL  = process.env.NODE_ENV === "production" ? "https://grab-market-server-mingddo.fly.dev" : "http://localhost:8080";
